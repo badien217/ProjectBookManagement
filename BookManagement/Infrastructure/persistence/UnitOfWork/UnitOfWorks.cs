ca76be811fa0985fc3ac191e1880c88395e5a214
vubadien <badien2172004@gmail.com>
2024-06-17 17:06:41 +0700

    unit of work

---
+++ BookManagement/Infrastructure/persistence/UnitOfWork/UnitOfWorks.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces.Reponsitories;
+using Application.Interfaces.UnitOfWork;
+using persistence.context;
+using persistence.Reponsitory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persistence.UnitOfWork
+{
+    public class UnitOfWorks : IUnitOfWork
+    {
+        private readonly AddDbContext _dbContext;
+        public UnitOfWorks(AddDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public async ValueTask DisposeAsync()=> await _dbContext.DisposeAsync();
+
+        public int Save() => _dbContext.SaveChanges();
+        
+
+        public async Task<int> SaveAsync() => await _dbContext.SaveChangesAsync();
+
+
+        IReadReponsitory<T> IUnitOfWork.GetReadReponsitory<T>() => new ReadReponsitory<T>(_dbContext);
+
+
+        IWriteReponsitory<T> IUnitOfWork.GetWriteReponsitory<T>() => new WriteReponsitory<T>(_dbContext);
+        
+    }
+}
