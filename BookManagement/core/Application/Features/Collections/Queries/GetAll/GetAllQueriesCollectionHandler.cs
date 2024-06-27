976e91e6586b4de58d2631ed757ae11ae0eb948d
vubadien <badien2172004@gmail.com>
2024-06-27 21:28:05 +0700

    GetAllCollection

---
+++ BookManagement/core/Application/Features/Collections/Queries/GetAll/GetAllQueriesCollectionHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Base;
+using Application.Dtos;
+using Application.Interfaces.AutoMapping;
+using Application.Interfaces.UnitOfWork;
+using Domain.Entity;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Collections.Queries.GetAll
+{
+    public class GetAllQueriesCollectionHandler : BaseHandler, IRequestHandler<GetAllQueriesCollectionRequest, IList<GetAllQueriesCollectionReponse>>
+    {
+        public GetAllQueriesCollectionHandler(IAutoMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+        }
+
+        public async Task<IList<GetAllQueriesCollectionReponse>> Handle(GetAllQueriesCollectionRequest request, CancellationToken cancellationToken)
+        {
+            var collection = await unitOfWork.GetReadReponsitory<Collection>().GetAllAsync(b => !b.IsDeleted,include: x => x.Include(y => y.sale)  );\
+            var map = mapper.Map<AuthorDtos, Author>(new Author());
+            var collectionmap = mapper.Map<GetAllQueriesCollectionReponse, Collection>(collection);
+            return collectionmap;
+        }
+    }
+}
---
+++ BookManagement/core/Application/Features/Collections/Queries/GetAll/GetAllQueriesCollectionReponse.cs
@@ -0,0 +1,16 @@
+﻿using Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Collections.Queries.GetAll
+{
+    public class GetAllQueriesCollectionReponse
+    {
+        public string name { get; set; }
+        public string content { get; set; }
+        public SaleDtos SaleDtos { get; set; }
+    }
+}
