65f019a0ff0fecea56edb2707a83c08fe747de43
vubadien <badien2172004@gmail.com>
2024-08-02 00:54:48 +0700

    front end

---
+++ BookManagement/core/Domain/Entity/Category.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Category : Entitybase,IEntitybase
+    {
+        public string name { get;set; }
+        public string image { get; set; }
+    }
+}
---
+++ BookManagement/core/Domain/Entity/Product.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Product : Entitybase,IEntitybase
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public float sellingPrice { get; set; }
+        public float mrp { get; set; }
+        public int itemQuantityType { get; set; }
+    }
+}
